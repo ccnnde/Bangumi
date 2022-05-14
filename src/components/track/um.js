95d3aa1949f835c9aaa3684d61a16f11e9c079a3
czy0729 <402731062@qq.com>
2022-05-14 19:09:52 +0800

    - rm UMAnalyticsModule

--- src/components/track/um.js
+++ src/components/track/um.js
@@ -4,31 +4,34 @@
  * @Author: czy0729
  * @Date: 2019-11-26 20:10:57
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-03-14 21:17:39
+ * @Last Modified time: 2022-05-14 19:08:50
  */
-import React from 'react'
-import { NativeModules } from 'react-native'
-import { IOS } from '@constants'
-import { NavigationEvents } from '../navigation/events'
+// import React from 'react'
+// import { NativeModules } from 'react-native'
+// import { IOS } from '@constants'
+// import { NavigationEvents } from '../navigation/events'
 
-const { UMAnalyticsModule } = NativeModules
+// const { UMAnalyticsModule } = NativeModules
 
-function UM({
-  /* 页面的名称 (中文) */
-  title = ''
-}) {
-  if (IOS || !title) return null
-  return (
-    <NavigationEvents
-      onDidFocus={() => {
-        // setTimeout是保证页面开始记录是在上个页面结束之后才执行
-        setTimeout(() => UMAnalyticsModule.onPageStart(title), 0)
-      }}
-      onWillBlur={() => {
-        UMAnalyticsModule.onPageEnd(title)
-      }}
-    />
-  )
+function UM(
+  {
+    /* 页面的名称 (中文) */
+    // title = ''
+  }
+) {
+  return null
+  // if (IOS || !title) return null
+  // return (
+  //   <NavigationEvents
+  //     onDidFocus={() => {
+  //       // setTimeout是保证页面开始记录是在上个页面结束之后才执行
+  //       setTimeout(() => UMAnalyticsModule.onPageStart(title), 0)
+  //     }}
+  //     onWillBlur={() => {
+  //       UMAnalyticsModule.onPageEnd(title)
+  //     }}
+  //   />
+  // )
 }
 
 export default UM
--- src/components/um.js
+++ src/components/um.js
@@ -2,26 +2,31 @@
  * @Author: czy0729
  * @Date: 2019-11-26 20:10:57
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-03-12 04:32:30
+ * @Last Modified time: 2022-05-14 19:09:12
  */
-import React from 'react'
-import { NativeModules } from 'react-native'
-import { IOS } from '@constants'
-import { NavigationEvents } from './navigation/events'
+// import React from 'react'
+// import { NativeModules } from 'react-native'
+// import { IOS } from '@constants'
+// import { NavigationEvents } from './navigation/events'
 
-const { UMAnalyticsModule } = NativeModules
+// const { UMAnalyticsModule } = NativeModules
 
-export const UM = ({ screen = '' }) => {
-  if (IOS || !screen) return null
+export const UM = (
+  {
+    // screen = ''
+  }
+) => {
+  return null
+  // if (IOS || !screen) return null
 
-  return (
-    <NavigationEvents
-      onDidFocus={() => {
-        setTimeout(() => UMAnalyticsModule.onPageStart(screen), 0)
-      }}
-      onWillBlur={() => {
-        UMAnalyticsModule.onPageEnd(screen)
-      }}
-    />
-  )
+  // return (
+  //   <NavigationEvents
+  //     onDidFocus={() => {
+  //       setTimeout(() => UMAnalyticsModule.onPageStart(screen), 0)
+  //     }}
+  //     onWillBlur={() => {
+  //       UMAnalyticsModule.onPageEnd(screen)
+  //     }}
+  //   />
+  // )
 }
